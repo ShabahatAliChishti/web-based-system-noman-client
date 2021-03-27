b41fc4f1fcd54240ffc720c8b1e31157bb4594e3
shabahat97 <shabahatalichishti@gmail.com>
2021-03-27 17:16:23 +0500

    shabahat

---
+++ WebBasedSystem/Controllers/StandardUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBasedSystem.Controllers
+{
+    public class StandardUserController : Controller
+    {
+        // GET: StandardUser
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
